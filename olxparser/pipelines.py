9416858949cadbef1e61332bc7e593be566fd4b2
Sblvsk <smr.kolesnikov@gmail.com>
2022-07-23 14:26:37 +0400

    pull

---
+++ olxparser/pipelines.py
@@ -0,0 +1,38 @@
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+
+
+# useful for handling different item types with a single interface
+import scrapy
+from itemadapter import ItemAdapter
+from scrapy.pipelines.images import ImagesPipeline
+from pymongo import MongoClient
+
+class OlxparserPipeline:
+
+    def __init__(self):
+        client = MongoClient('127.0.0.1', 27017)
+        self.mongo_base = client.job_openings
+
+    def process_item(self, item, spider):
+        collection = self.mongo_base[spider.name]
+        collection.insert_one(item)
+        return item
+
+class OlxPhotosPipeline(ImagesPipeline):
+    def get_media_requests(self, item, info):
+        if item['photos']:
+            for img in item['photos']:
+                try:
+                    yield scrapy.Request(img)
+                except Exception as e:
+                    print(e)
+
+    def item_completed(self, results, item, info):
+        item['photos'] = [itm[1] for itm in results if itm[0]]
+        return item
+
+    # def file_path(self, request, response=None, info=None, *, item=None):
+    #     return ''
